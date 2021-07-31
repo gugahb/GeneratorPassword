abcc937556e5cce207506f621c165487391f220a
Gustavo Borges <gustavohbcn@gmail.com>
2021-07-31 09:52:42 -0300

    send my file to github

---
+++ genPass.py
@@ -0,0 +1,34 @@
+#Criando meu próprio gerador de senhas em Python
+
+import random
+
+#aqui coloque os caracteres minusculos, caso não queira algum só remover
+min = 'abcdefghijklmnopqrstuvwxyz'
+#aqui coloque os caracteres MAIUSCULOS, caso não queira algum só remover
+max = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#aqui coloque os números caso não queira algum só remover
+num = '0123456789'
+#aqui coloque os símbolos caso não queira algum só remover
+sybs = '[]{}()*#;/,-_%'
+
+#altere o length para a quantidade de caracteres que você queira
+length = 20
+
+#fazendo senha com todos
+all = min + max + num + sybs
+passwordAll = "".join(random.sample(all,length))
+
+#só maiúsculas e números
+MAXnum = max + num
+passwordMAXnum = "".join(random.sample(MAXnum,length))
+
+#só minusculas e maiúsculas
+MAXmin = max + min
+passwordMAXmin = "".join(random.sample(MAXmin,length))
+
+# pronto, basta executar o seu código
+print ('passwordAll = ' + passwordAll) 
+print ('passwordMAXnum = ' + passwordMAXnum) 
+print ('passwordMAXmin = ' + passwordMAXmin)
+
+#Crie a sua combinação como quiser
